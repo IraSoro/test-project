4bf7c620a095f0fe4958f3f2be7ed733fbcf1af5
Irina Sorokina <irasoro402@yandex.ru>
2025-04-09 22:23:44 +0400

    Rewrote `useSort.tsx`

--- client/src/pages/useSort.ts
+++ client/src/pages/useSort.ts
@@ -1,28 +1,23 @@
 import { useMemo, useState } from "react";
+import { ItemType } from "../data/item-type";
 
-function useSort(items: any[]): [any[], any, any] {
+function useSort(items: ItemType[]): [ItemType[], string, () => void] {
   const [sortBy, setSortBy] = useState("ASC");
 
   const sortedItems = useMemo(() => {
     if (sortBy === "DESC") {
-      return items;
+      return items.sort((a, b) => b.id - a.id);
     }
 
     if (sortBy === "ASC") {
-      return items.sort((a, b) => b.id - a.id);
+      return items.sort((a, b) => a.id - b.id);
     }
 
     return items;
   }, [items, sortBy]);
 
   const handleSortClick = () => {
-    if (sortBy === "ASC") {
-      setSortBy("DESC");
-    } else if (sortBy === "DESC") {
-      setSortBy("ASC");
-    } else {
-      setSortBy("");
-    }
+    setSortBy(sortBy === "ASC" ? "DESC" : sortBy === "DESC" ? "ASC" : "");
   };
 
   return [sortedItems, sortBy, handleSortClick];
